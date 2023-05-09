ea1778abe3b9e1d6a18e1bf7ec669cdedcee65be
Renato Sampaio Albuquerque <renatosampaio81@yahoo.com.br>
2023-05-09 13:41:16 -0400

    week1 - classes java hands on

---
+++ Week1/Comercio/src/Compra.java
@@ -0,0 +1,32 @@
+// Trabalhando com mais de um construtor
+
+public class Compra {
+
+	int valorTotal;
+	int numeroParcelas;
+	
+	//compra a vista
+	public Compra(int valor) {
+		valorTotal = valor;
+		numeroParcelas = 1;
+	}
+	
+	//compra parcelada
+	public Compra(int qtdParcelas, int valorParcela) {
+		numeroParcelas = qtdParcelas;
+		valorTotal = valorParcela * qtdParcelas;
+	}
+
+	public int getValorTotal() {
+		return valorTotal;
+	}
+
+	public int getNumeroParcelas() {
+		return numeroParcelas;
+	}
+	
+	public int getValorParcela() {
+		return valorTotal/numeroParcelas;
+	}
+	
+}
