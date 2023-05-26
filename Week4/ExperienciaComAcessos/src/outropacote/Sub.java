87aa9c031889ba391d1ccb2c5abad9f6cf5c8a89
Renato Sampaio Albuquerque <renatosampaio81@yahoo.com.br>
2023-05-26 08:24:43 -0400

    week4 - Acesso aos Objetos

---
+++ Week4/ExperienciaComAcessos/src/outropacote/Sub.java
@@ -0,0 +1,14 @@
+package outropacote;
+
+import pacote.Referencia;
+
+public class Sub extends Referencia {
+	
+	public void testeAcesso() {
+		//modificadorDefault = 0;
+		modificadorPublico = 0;
+		modificadorProtegido = 0;
+		//modificadorPrivado = 0;
+	}
+
+}
